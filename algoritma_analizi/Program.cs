b509eb7eb57f59953efcb516d64768ed51fdf1b4
yldrmahmet <ay689089@gmail.com>
2025-03-03 13:09:03 +0300

    ikinci hafta

---
+++ algoritma_analizi/Program.cs
@@ -0,0 +1,37 @@
+﻿internal class Program
+{
+    void sort(int[] arr)
+    {
+        int n = arr.Length;
+        for (int i = 1; i < n; ++i)
+        {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key)
+            {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    /* A utility function to print array of size n */
+    static void printArray(int[] arr)
+    {
+        int n = arr.Length;
+        for (int i = 0; i < n; ++i)
+            Console.Write(arr[i] + " ");
+
+        Console.WriteLine();
+    }
+
+    // Driver method
+    public static void Main()
+    {
+        int[] arr = { 12, 11, 13, 5, 6 };
+        sort(arr);
+
+        printArray(arr);
+    }
+}
